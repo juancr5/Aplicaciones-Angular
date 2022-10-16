56aa99572c49d15112af248f0a38357e92d3b863
Juan Camilo <37746221+juancr5@users.noreply.github.com>
2022-10-16 13:42:34 -0500

    Update app.component.ts

--- 01-piano-color/src/app/app.component.ts
+++ 01-piano-color/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'piano-color';
 
-  //recibe un un numero enteri y devuelve un sonido de acuerdo al los sonidos de la lista 
+  //recibe un un numero entero y devuelve un sonido de acuerdo al los sonidos de la lista 
   aplicarSonido(numero: number): void {
     //crea un objeto de tipo Audio
     const audio = new Audio();
